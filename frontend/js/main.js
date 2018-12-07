edb89b5276b783006bebfcbb80cc5a6be8a99c3d
Deeksha Bajaj <deekshabajaj@Deekshas-MacBook-Pro.local>
2018-12-07 09:00:35 +0900

    changes in front-end booking page

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,25 +3,6 @@
   "use strict";
 
 /* 
-   Touch Owl Carousel
-   ========================================================================== */
-    var owl = $(".touch-slider");
-    owl.owlCarousel({
-      navigation: false,
-      pagination: true,
-      slideSpeed: 1000,
-      stopOnHover: true,
-      autoPlay: true,
-      items: 1,
-      itemsDesktopSmall: [1024, 1],
-      itemsTablet: [600, 1],
-      itemsMobile: [479, 1]
-    });
-
-    $('.touch-slider').find('.owl-prev').html('<i class="fa fa-chevron-left"></i>');
-    $('.touch-slider').find('.owl-next').html('<i class="fa fa-chevron-right"></i>');
-
-/* 
    Sticky Nav
    ========================================================================== */
     $(window).on('scroll', function() {
