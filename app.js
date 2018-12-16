77abb0572da19c8ae635545edde22444ea0f95fa
Raunaq Sachdev <raunaq@canva.com>
2018-12-16 21:29:31 +1100

    Vue frontend (#4)
     Initial Working Draft
    Includes all front end code till now

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 // const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
+const bookingRouter = require('./routes/booking');
 
 const app = express();
 
@@ -13,12 +14,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public/static/')));
 
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/booking', bookingRouter);
 
 module.exports = app;
